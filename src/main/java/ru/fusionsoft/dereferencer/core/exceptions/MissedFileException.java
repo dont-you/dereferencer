acc61ca772d4fc3a1d51100a9fee0786b7885f07
dont-you <mihailwhoss@gmail.com>
2023-07-05 18:26:48 +0300

    feat: add NotSupportedSourceType and MissedFileException

---
+++ src/main/java/ru/fusionsoft/dereferencer/core/exceptions/MissedFileException.java
@@ -0,0 +1,7 @@
+package ru.fusionsoft.dereferencer.core.exceptions;
+
+public class MissedFileException extends LoadException {
+    public MissedFileException(String msg) {
+        super(msg);
+    }
+}
