f5b28fd96e51f76be2034819569c6c497fb06601
dont-you <mihailwhoss@gmail.com>
2023-07-05 18:30:17 +0300

    fix: name of directory with test files

--- src/integration-test/java/ru.fusionsoft.dereferencer/DereferencerIT.java
+++ src/integration-test/java/ru.fusionsoft.dereferencer/DereferencerIT.java
@@ -3,14 +3,21 @@ package ru.fusionsoft.dereferencer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.google.common.base.Throwables;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 import ru.fusionsoft.dereferencer.core.exceptions.LoadException;
+import ru.fusionsoft.dereferencer.core.exceptions.URIException;
+import ru.fusionsoft.dereferencer.core.exceptions.UnknownException;
 
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
 
 public class DereferencerIT {
 
@@ -20,19 +27,16 @@ public class DereferencerIT {
     public void Test_simple_scheme_With_url_reference() throws LoadException, IOException {
         Dereferencer dereferencer = new Dereferencer(DereferenceConfiguration.builder().build());
         JsonNode actual = dereferencer
-            .dereference(URI.create("./src/test/resources/test-schemes/schemes/simple_scheme_with_url_ref.json"));
+            .dereference(URI.create("./src/integration-test/resources/test-schemes/schemes/simple_scheme_with_url_ref.json"));
         JsonNode expected = jsonMapper.readTree(Paths
-                                            .get("./src/test/resources/test-schemes/expected-result/dereferenced_simple_scheme_with_url_ref.json")
+                                            .get("./src/integration-test/resources/test-schemes/expected-result/dereferenced_simple_scheme_with_url_ref.json")
                                             .toFile());
 
         assertEquals(expected, actual);
    }
 
-   @Test
-   public void delme() throws LoadException, IOException {
-        JsonNode jsonNode = jsonMapper.readTree("{\"allOf\":{}}");
-        if(!jsonNode.at("/allOf").isMissingNode()){
-            System.out.println("allof schema node");
-        }
-   }
+    @Test
+    public void delme(){
+
+    }
 }
