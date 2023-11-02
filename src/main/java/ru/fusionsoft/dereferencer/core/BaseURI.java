a92aaa03cb98425a81d7c268ea91904e702f12a8
dont-you <mihailwhoss@gmail.com>
2023-11-03 02:11:28 +0300

    feat: write BaseURI class

--- src/main/java/ru/fusionsoft/dereferencer/core/BaseURI.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/BaseURI.java
@@ -1,4 +1,31 @@
 package ru.fusionsoft.dereferencer.core;
 
+import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
+
 public class BaseURI {
+    private URI canonical;
+    private Set<URI> duplicates;
+
+    public BaseURI(URI canonical){
+        duplicates = new HashSet<>();
+        updateCanonical(canonical);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(getClass() != obj.getClass())
+            return false;
+        return duplicates.contains(obj);
+    }
+
+    public void updateCanonical(URI canonical){
+        this.canonical = canonical;
+        duplicates.add(canonical);
+    }
+
+    public URI getCanonical(){
+        return canonical;
+    }
 }
--- src/main/java/ru/fusionsoft/dereferencer/core/FileRegister.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/FileRegister.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.net.URI;
 
 public class FileRegister {
+    public FileRegister(URNPool urnPool, LoaderFactory loaderFactory, FileFactory fileFactory, URI defaultBaseURI){
+        // TODO
+    }
+
     public File get(URI uri){
         // TODO
         return null;
