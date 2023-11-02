3660a1e65469b293928af8b9c9e1d8787b374db4
dont-you <mihailwhoss@gmail.com>
2023-11-02 17:25:02 +0300

    feat: write DereferencerBuilder class

--- src/main/java/ru/fusionsoft/dereferencer/Dereferencer.java
+++ src/main/java/ru/fusionsoft/dereferencer/Dereferencer.java
@@ -2,9 +2,17 @@ package ru.fusionsoft.dereferencer;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import ru.fusionsoft.dereferencer.core.FileFactory;
+import ru.fusionsoft.dereferencer.core.LoaderFactory;
+import ru.fusionsoft.dereferencer.core.URNPool;
+
 import java.net.URI;
 
 public class Dereferencer {
+    public Dereferencer(URNPool urnPool, LoaderFactory loaderFactory, FileFactory fileFactory, URI defaultBaseURI){
+        // TODO
+    }
+
     public JsonNode dereference(URI uri){
         // TODO
         return null;
--- src/main/java/ru/fusionsoft/dereferencer/DereferencerBuilder.java
+++ src/main/java/ru/fusionsoft/dereferencer/DereferencerBuilder.java
@@ -1,17 +1,57 @@
 package ru.fusionsoft.dereferencer;
 
-public class DereferencerBuilder {
-    private DereferencerBuilder(){
+import java.net.URI;
+import java.nio.file.Paths;
 
+import ru.fusionsoft.dereferencer.allOf.AllOfFileFactory;
+import ru.fusionsoft.dereferencer.core.FileFactory;
+import ru.fusionsoft.dereferencer.core.LoaderFactory;
+import ru.fusionsoft.dereferencer.core.URNPool;
+import ru.fusionsoft.dereferencer.core.impl.file.BaseFileFactory;
+import ru.fusionsoft.dereferencer.core.impl.load.BaseLoaderFactory;
+import ru.fusionsoft.dereferencer.core.impl.urn.BaseUrnPool;
+
+public class DereferencerBuilder {
+    private URNPool urnPool;
+    private LoaderFactory loaderFactory;
+    private FileFactory fileFactory;
+    private URI defaultBaseURI;
+
+    private DereferencerBuilder(){
+        setUrnPool(new BaseUrnPool()).setLoaderFactory(new BaseLoaderFactory()).setFileFactory(new BaseFileFactory())
+                .setDefaultBaseURI(Paths.get(".").toAbsolutePath().normalize().toUri());
     }
 
     public static DereferencerBuilder builder(){
-        // TODO
-        return null;
+        return new DereferencerBuilder();
     }
 
     public Dereferencer build(){
-        // TODO
-        return null;
+        return new Dereferencer(urnPool, loaderFactory, fileFactory, defaultBaseURI);
+    }
+
+    public DereferencerBuilder enableAllOfMerge(){
+        setFileFactory(new AllOfFileFactory());
+        return this;
+    }
+
+    public DereferencerBuilder setUrnPool(URNPool urnPool){
+        this.urnPool = urnPool;
+        return this;
+    }
+
+    public DereferencerBuilder setLoaderFactory(LoaderFactory loaderFactory){
+        this.loaderFactory = loaderFactory;
+        return this;
+    }
+
+    public DereferencerBuilder setFileFactory(FileFactory fileFactory){
+        this.fileFactory = fileFactory;
+        return this;
+    }
+
+    public DereferencerBuilder setDefaultBaseURI(URI defaultBaseURI){
+        this.defaultBaseURI = defaultBaseURI;
+        return this;
     }
 }
