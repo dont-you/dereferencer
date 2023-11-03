a0d206729fc4f25e1bd3e6ca884c86a98fea9eb7
dont-you <mihailwhoss@gmail.com>
2023-11-03 04:40:49 +0300

    feat: write BaseLoaderFactory

--- src/main/java/ru/fusionsoft/dereferencer/core/LoaderFactory.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/LoaderFactory.java
@@ -2,6 +2,8 @@ package ru.fusionsoft.dereferencer.core;
 
 import java.net.URI;
 
+import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
+
 public interface LoaderFactory {
-    public SourceLoader getSourceLoader(URI uri);
+    public SourceLoader getSourceLoader(URI uri) throws DereferenceException;
 }
--- src/main/java/ru/fusionsoft/dereferencer/core/impl/load/BaseLoaderFactory.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/impl/load/BaseLoaderFactory.java
@@ -2,14 +2,22 @@ package ru.fusionsoft.dereferencer.core.impl.load;
 
 import ru.fusionsoft.dereferencer.core.LoaderFactory;
 import ru.fusionsoft.dereferencer.core.SourceLoader;
+import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
 
 import java.net.URI;
 
 public class BaseLoaderFactory implements LoaderFactory {
 
+    private LocalLoader localLoader;
+    private URLLoader urlLoader;
+
     @Override
-    public SourceLoader getSourceLoader(URI uri) {
-        // TODO
-        return null;
+    public SourceLoader getSourceLoader(URI uri) throws DereferenceException {
+        if (localLoader.canLoad(uri))
+            return localLoader;
+        else if (urlLoader.canLoad(uri))
+            return urlLoader;
+        else
+            throw new DereferenceException("source loader for resource with uri " + uri + " is not implemented");
     }
 }
