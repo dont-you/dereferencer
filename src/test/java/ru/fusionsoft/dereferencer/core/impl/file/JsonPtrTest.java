93bc5ff8a61b6bc73e0e3233d42dbd984fdd9e24
dont-you <mihailwhoss@gmail.com>
2023-11-24 18:26:20 +0300

    fix: test for JsonPtr

--- src/test/java/ru/fusionsoft/dereferencer/core/impl/file/JsonPtrTest.java
+++ src/test/java/ru/fusionsoft/dereferencer/core/impl/file/JsonPtrTest.java
@@ -1,6 +1,8 @@
-package ru.fusionsoft.dereferencer.core;
+package ru.fusionsoft.dereferencer.core.impl.file;
 
 import org.junit.Test;
+import ru.fusionsoft.dereferencer.core.impl.file.JsonPtr;
+
 import static org.junit.Assert.*;
 
 public class JsonPtrTest {
