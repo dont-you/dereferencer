d1533af86ee81e3f5c4819f75bc3dcba4d7c62af
dont-you <mihailwhoss@gmail.com>
2023-12-15 17:12:35 +0300

    feat: GitLoaderFactory logic

--- src/main/java/ru/fusionsoft/dereferencer/git/GitLoaderFactory.java
+++ src/main/java/ru/fusionsoft/dereferencer/git/GitLoaderFactory.java
@@ -1,4 +1,38 @@
 package ru.fusionsoft.dereferencer.git;
 
-public class GitLoaderFactory {
+import ru.fusionsoft.dereferencer.core.LoaderFactory;
+import ru.fusionsoft.dereferencer.core.SourceLoader;
+import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
+import ru.fusionsoft.dereferencer.core.impl.load.BaseLoaderFactory;
+
+import java.net.URL;
+
+public class GitLoaderFactory implements LoaderFactory {
+    private final BaseLoaderFactory baseLoaderFactory;
+    private final GitHubLoader gitHubLoader;
+    private final GitLabLoader gitLabLoader;
+
+    public GitLoaderFactory(){
+        this.baseLoaderFactory = new BaseLoaderFactory();
+        this.gitHubLoader = new GitHubLoader();
+        this.gitLabLoader = new GitLabLoader();
+    }
+
+    public GitHubLoader getGitHubLoader() {
+        return gitHubLoader;
+    }
+
+    public GitLabLoader getGitLabLoader() {
+        return gitLabLoader;
+    }
+
+    @Override
+    public SourceLoader getSourceLoader(URL url) throws DereferenceException {
+        if(gitHubLoader.canLoad(url))
+            return gitHubLoader;
+        else if(gitLabLoader.canLoad(url))
+            return gitLabLoader;
+        else
+            return baseLoaderFactory.getSourceLoader(url);
+    }
 }
