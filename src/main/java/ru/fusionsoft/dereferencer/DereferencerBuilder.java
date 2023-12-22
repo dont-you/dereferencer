a238c2eb3e770d49d98edae1f12757f7b6b6a227
dont-you <mihailwhoss@gmail.com>
2023-12-22 14:22:24 +0300

    style: DereferencerBuilder class

--- src/main/java/ru/fusionsoft/dereferencer/DereferencerBuilder.java
+++ src/main/java/ru/fusionsoft/dereferencer/DereferencerBuilder.java
@@ -17,40 +17,40 @@ public class DereferencerBuilder {
     private FileFactory fileFactory;
     private URI defaultBaseURI;
 
-    private DereferencerBuilder(){
+    private DereferencerBuilder() {
         setUrnPool(new TagURIPool()).setLoaderFactory(new BaseLoaderFactory()).setFileFactory(new BaseFileFactory())
                 .setDefaultBaseURI(Paths.get(".").toAbsolutePath().normalize().toUri());
     }
 
-    public static DereferencerBuilder builder(){
+    public static DereferencerBuilder builder() {
         return new DereferencerBuilder();
     }
 
-    public Dereferencer build(){
+    public Dereferencer build() {
         return new Dereferencer(urnPool, loaderFactory, fileFactory, defaultBaseURI);
     }
 
-    public DereferencerBuilder enableAllOfMerge(){
+    public DereferencerBuilder enableAllOfMerge() {
         setFileFactory(new AllOfFileFactory());
         return this;
     }
 
-    public DereferencerBuilder setUrnPool(URNPool urnPool){
+    public DereferencerBuilder setUrnPool(URNPool urnPool) {
         this.urnPool = urnPool;
         return this;
     }
 
-    public DereferencerBuilder setLoaderFactory(LoaderFactory loaderFactory){
+    public DereferencerBuilder setLoaderFactory(LoaderFactory loaderFactory) {
         this.loaderFactory = loaderFactory;
         return this;
     }
 
-    public DereferencerBuilder setFileFactory(FileFactory fileFactory){
+    public DereferencerBuilder setFileFactory(FileFactory fileFactory) {
         this.fileFactory = fileFactory;
         return this;
     }
 
-    public DereferencerBuilder setDefaultBaseURI(URI defaultBaseURI){
+    public DereferencerBuilder setDefaultBaseURI(URI defaultBaseURI) {
         this.defaultBaseURI = defaultBaseURI;
         return this;
     }
