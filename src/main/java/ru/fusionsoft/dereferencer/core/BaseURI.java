6974fa2b382639bb21945f6a7c330eab03dc508a
dont-you <mihailwhoss@gmail.com>
2023-12-22 15:04:15 +0300

    style: BaseURI class

--- src/main/java/ru/fusionsoft/dereferencer/core/BaseURI.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/BaseURI.java
@@ -4,22 +4,22 @@ import java.net.URI;
 import java.util.HashSet;
 import java.util.Set;
 
-public class BaseURI implements Comparable<BaseURI>{
+public class BaseURI implements Comparable<BaseURI> {
     private URI canonical;
     private final Set<URI> duplicates;
 
-    public BaseURI(URI defaultBaseURI, URI canonical){
+    public BaseURI(URI defaultBaseURI, URI canonical) {
         duplicates = new HashSet<>();
         this.canonical = defaultBaseURI;
         updateCanonical(canonical);
     }
 
-    public void updateCanonical(URI canonical){
+    public void updateCanonical(URI canonical) {
         this.canonical = this.canonical.resolve(canonical);
         duplicates.add(this.canonical);
     }
 
-    public URI getCanonical(){
+    public URI getCanonical() {
         return canonical;
     }
 
@@ -27,7 +27,7 @@ public class BaseURI implements Comparable<BaseURI>{
     public int compareTo(BaseURI arg0) {
         if (duplicates.contains(arg0.canonical))
             return 0;
-        else if(duplicates.size() >= arg0.duplicates.size())
+        else if (duplicates.size() >= arg0.duplicates.size())
             return 1;
         else
             return -1;
