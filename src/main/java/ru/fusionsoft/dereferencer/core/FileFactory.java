3f48505fa176341341482a48d31a9d27e25ffd11
dont-you <mihailwhoss@gmail.com>
2023-12-22 15:14:13 +0300

    refactor: remove unnecessary modifier from interface

--- src/main/java/ru/fusionsoft/dereferencer/core/FileFactory.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/FileFactory.java
@@ -7,5 +7,5 @@ import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
 import java.net.URI;
 
 public interface FileFactory {
-    public File makeFile(FileRegister fileRegister, URI baseURI, JsonNode source) throws DereferenceException;
+    File makeFile(FileRegister fileRegister, URI baseURI, JsonNode source) throws DereferenceException;
 }
