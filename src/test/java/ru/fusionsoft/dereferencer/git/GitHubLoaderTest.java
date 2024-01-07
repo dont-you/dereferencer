9e8790b54eec23b8376d0a5919dc6b1cb542d723
dont-you <mihailwhoss@gmail.com>
2024-01-07 12:42:15 +0300

    test: refactor GitHubLoader unit test

--- src/test/java/ru/fusionsoft/dereferencer/git/GitHubLoaderTest.java
+++ src/test/java/ru/fusionsoft/dereferencer/git/GitHubLoaderTest.java
@@ -1,26 +1,64 @@
 package ru.fusionsoft.dereferencer.git;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
-import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
+
+import org.junit.runner.RunWith;
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
+
+@RunWith(MockitoJUnitRunner.class)
 public class GitHubLoaderTest {
 
-    private GitHubLoader gitHubLoader = new GitHubLoader();
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Mock
+    GitHub mockedGitHub;
+    @Mock
+    GHRepository mockedRepo;
 
-    public GitHubLoaderTest() throws IOException {
+    @Mock
+    GHContent mockedContent;
+
+    @Mock
+    InputStream expectedStream;
+
+    private GitHubLoader gitHubLoader;
+
+    @Before
+    public void init() throws IOException {
+        gitHubLoader = new GitHubLoader();
+        gitHubLoader.configureGitHubLoader(mockedGitHub);
     }
 
     @Test
-    public void Test_load_json_file() throws DereferenceException, IOException {
-        JsonNode node = objectMapper.readTree(gitHubLoader.loadSource(new URL("https://github.com/dont-you/tests/blob/main/test.json")));
-        assertEquals(node, objectMapper.readTree("\"test-complete\""));
+    public void Test_load_json_file() throws IOException {
+        testLoadMethod(new URL("https://github.com/dont-you/tests/blob/main/test.json"), "dont-you/tests","test.json","main");
+        testLoadMethod(new URL("https://github.com/dont-you/tests/blob/main/path/to/test.json"), "dont-you/tests","path/to/test.json","main");
+        testLoadMethod(new URL("https://github.com/dont-you/tests/raw/main/test.json"), "dont-you/tests","test.json","main");
+    }
+
+    private void testLoadMethod(URL url, String projectPath, String filePath, String ref) throws IOException {
+        initGitHubMocs(projectPath, filePath, ref);
+
+        InputStream actualStream = gitHubLoader.loadSource(url);
+        assertEquals(expectedStream, actualStream);
+    }
+    private void initGitHubMocs(String projectPath, String filePath, String ref) throws IOException {
+        Mockito.when(mockedGitHub.getRepository(projectPath)).thenReturn(mockedRepo);
+        Mockito.when(mockedRepo.getFileContent(filePath, ref)).thenReturn(mockedContent);
+        Mockito.when(mockedContent.read()).thenReturn(expectedStream);
     }
 }
