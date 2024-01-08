b13e7e20e3952acc2ff65d42905ac02211a8cf73
dont-you <mihailwhoss@gmail.com>
2024-01-08 16:05:47 +0300

    feat: add unchecked exception

---
+++ src/main/java/ru/fusionsoft/dereferencer/core/exceptions/DereferenceRuntimeException.java
@@ -0,0 +1,11 @@
+package ru.fusionsoft.dereferencer.core.exceptions;
+
+public class DereferenceRuntimeException extends RuntimeException {
+    public DereferenceRuntimeException(String msg) {
+        super(msg);
+    }
+
+    public DereferenceRuntimeException(String msg,Throwable t) {
+        super(msg,t);
+    }
+}
