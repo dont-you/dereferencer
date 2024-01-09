31194f6589418407fd85a97328e4af926bd167d7
dont-you <mihailwhoss@gmail.com>
2024-01-09 03:13:59 +0300

    fix: Reference constructor throws if fragmentIdentifier null

--- src/main/java/ru/fusionsoft/dereferencer/core/impl/file/Reference.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/impl/file/Reference.java
@@ -1,12 +1,17 @@
 package ru.fusionsoft.dereferencer.core.impl.file;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
+import ru.fusionsoft.dereferencer.core.exceptions.DereferenceRuntimeException;
 
 public class Reference {
     private final FragmentIdentifier fragmentIdentifier;
     private JsonNode fragment;
 
     public Reference(FragmentIdentifier fragmentIdentifier) {
+        if(fragmentIdentifier==null)
+            throw new NullPointerException("fragment identifier should not be null");
+
         this.fragmentIdentifier = fragmentIdentifier;
         fragment = null;
     }
