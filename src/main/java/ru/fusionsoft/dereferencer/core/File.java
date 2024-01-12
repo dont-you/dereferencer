51affcc2ac03b218f6f6a75d3851c071bf6ca9a2
dont-you <mihailwhoss@gmail.com>
2024-01-12 16:51:01 +0300

    refactor: rename 'dereference' method to 'resolve'

--- src/main/java/ru/fusionsoft/dereferencer/core/File.java
+++ src/main/java/ru/fusionsoft/dereferencer/core/File.java
@@ -2,12 +2,10 @@ package ru.fusionsoft.dereferencer.core;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
-import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
-
 public interface File {
     JsonNode getDerefedJson();
 
     JsonNode getSourceNode();
 
-    void dereference();
+    void resolve();
 }
