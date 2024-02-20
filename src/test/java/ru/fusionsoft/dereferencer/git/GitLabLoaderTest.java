ab8f3a796ddf7b23c0e69b98a466bfc56af055e5
dont-you <mihailwhoss@gmail.com>
2024-02-20 15:39:44 +0300

    test: fix git interaction tests

--- src/test/java/ru/fusionsoft/dereferencer/git/GitLabLoaderTest.java
+++ src/test/java/ru/fusionsoft/dereferencer/git/GitLabLoaderTest.java
@@ -10,8 +10,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import ru.fusionsoft.dereferencer.core.Resource;
+import ru.fusionsoft.dereferencer.core.exceptions.DereferenceException;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -19,39 +20,40 @@ import java.net.URISyntaxException;
 @RunWith(MockitoJUnitRunner.class)
 public class GitLabLoaderTest {
 
-//    @Mock
-//    GitLabApi mockedGitLab;
-//    @Mock
-//    RepositoryFileApi mockedRepo;
-//    @Mock
-//    RepositoryFile mockedFile;
-//    private GitLabLoader gitLabLoader;
-//
-//    @Before
-//    public void init() {
-//        gitLabLoader = new GitLabLoader();
-//        gitLabLoader.configureGitLabLoader(mockedGitLab);
-//    }
-//
-//    @Test
-//    public void Test_load_json_file() throws IOException, GitLabApiException, URISyntaxException {
-//        testLoadMethod(new URI("https://gitlab.com/dont-you/deref-test/-/blob/main/test.json"), "dont-you/deref-test", "test.json", "main");
-//        testLoadMethod(new URI("https://gitlab.com/dont-you/git-tests/-/blob/master/src/test.json"), "dont-you/git-tests", "src/test.json", "master");
-//    }
-//
-//    private void testLoadMethod(URI uri, String projectPath, String filePath, String ref) throws IOException, GitLabApiException {
-//        initGitLabMocs(projectPath, filePath, ref);
-//        gitLabLoader.loadSource(uri);
-//
-//        Mockito.verify(mockedGitLab, Mockito.times(1)).getRepositoryFileApi();
-//        Mockito.verify(mockedRepo, Mockito.times(1)).getFile(projectPath, filePath, ref);
-//        Mockito.verify(mockedFile, Mockito.times(1)).getDecodedContentAsBytes();
-//        Mockito.clearInvocations(mockedGitLab, mockedRepo, mockedFile);
-//    }
-//
-//    private void initGitLabMocs(String projectPath, String filePath, String ref) throws GitLabApiException {
-//        Mockito.when(mockedGitLab.getRepositoryFileApi()).thenReturn(mockedRepo);
-//        Mockito.when(mockedRepo.getFile(projectPath, filePath, ref)).thenReturn(mockedFile);
-//        Mockito.when(mockedFile.getDecodedContentAsBytes()).thenReturn(new byte[]{});
-//    }
+    @Mock
+    GitLabApi mockedGitLab;
+    @Mock
+    RepositoryFileApi mockedRepo;
+    @Mock
+    RepositoryFile mockedFile;
+    private GitLabLoader gitLabLoader;
+
+    @Before
+    public void init() {
+        gitLabLoader = new GitLabLoader();
+        gitLabLoader.configureGitLabLoader(mockedGitLab);
+    }
+
+    @Test
+    public void Test_load_json_file() throws GitLabApiException, URISyntaxException, DereferenceException {
+        testLoadMethod(new URI("https://gitlab.com/dont-you/deref-test/-/blob/main/test.json"), "dont-you/deref-test", "test.json", "main");
+        testLoadMethod(new URI("https://gitlab.com/dont-you/git-tests/-/blob/master/src/test.json"), "dont-you/git-tests", "src/test.json", "master");
+    }
+
+    private void testLoadMethod(URI uri, String projectPath, String filePath, String ref) throws GitLabApiException, DereferenceException {
+        initGitLabMocs(projectPath, filePath, ref);
+        gitLabLoader.load(new Resource(uri));
+
+        Mockito.verify(mockedGitLab, Mockito.times(1)).getRepositoryFileApi();
+        Mockito.verify(mockedRepo, Mockito.times(1)).getFile(projectPath, filePath, ref);
+        Mockito.verify(mockedFile, Mockito.times(1)).getDecodedContentAsBytes();
+        Mockito.clearInvocations(mockedGitLab, mockedRepo, mockedFile);
+    }
+
+    private void initGitLabMocs(String projectPath, String filePath, String ref) throws GitLabApiException {
+        Mockito.when(mockedGitLab.getRepositoryFileApi()).thenReturn(mockedRepo);
+        Mockito.when(mockedGitLab.getGitLabServerUrl()).thenReturn("https://gitlab.com");
+        Mockito.when(mockedRepo.getFile(projectPath, filePath, ref)).thenReturn(mockedFile);
+        Mockito.when(mockedFile.getDecodedContentAsBytes()).thenReturn(new byte[]{});
+    }
 }
