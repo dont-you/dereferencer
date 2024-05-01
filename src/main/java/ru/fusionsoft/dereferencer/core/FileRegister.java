2e7dddfde29eb97278c6de1997710d40bfc7a068
dont-you <mihailwhoss@gmail.com>
2024-05-01 16:13:04 +0300

    feat: add FileRegister class

---
+++ src/main/java/ru/fusionsoft/dereferencer/core/FileRegister.java
@@ -0,0 +1,65 @@
+package ru.fusionsoft.dereferencer.core;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLConnection;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FileRegister {
+    private final Map<URI, CachingFileProxy> cache;
+    private final DereferencedFileFactory dereferencedFileFactory;
+    private final Lock sameFileLock;
+
+    public FileRegister(DereferencedFileFactory fileFactory){
+        this.dereferencedFileFactory = fileFactory;
+        cache = new ConcurrentHashMap<>();
+        sameFileLock = new ReentrantLock();
+    }
+    public DereferencedFile get(URI uri) throws URISyntaxException, IOException {
+        CachingFileProxy initialProxy = null;
+
+        sameFileLock.lock();
+        if(!cache.containsKey(uri))
+            initialProxy = createAndPutToCache(uri);
+        sameFileLock.unlock();
+
+        return initialProxy==null ? cache.get(uri) : loadFile(uri, initialProxy);
+    }
+
+    private DereferencedFile loadFile(URI uri, CachingFileProxy initialProxy) throws URISyntaxException, IOException {
+        var proxies = new ArrayList<>(Collections.singletonList(initialProxy));
+        URLConnection urlConnection = uri.toURL().openConnection();
+
+        proxies.add(createAndPutToCache(urlConnection.getURL().toURI()));
+        configureProxies(proxies, makeFile(urlConnection, proxies));
+
+        return initialProxy;
+    }
+
+    private DereferencedFile makeFile(URLConnection urlConnection, List<CachingFileProxy> proxies) throws IOException, URISyntaxException {
+        DereferencedFile targetFile = dereferencedFileFactory.makeFile(urlConnection);
+        proxies.add(createAndPutToCache(targetFile.getBaseURI()));
+
+        return targetFile;
+    }
+
+    private void configureProxies(List<CachingFileProxy> proxies, DereferencedFile wrapping){
+        for(CachingFileProxy cachingFileProxy: proxies){
+            cachingFileProxy.setFile(wrapping);
+        }
+    }
+
+    private CachingFileProxy createAndPutToCache(URI uri){
+        CachingFileProxy cachingFileProxy = new CachingFileProxy();
+
+        if(!cache.containsKey(uri))
+            cache.put(uri, cachingFileProxy);
+
+        return cachingFileProxy;
+    }
+}
