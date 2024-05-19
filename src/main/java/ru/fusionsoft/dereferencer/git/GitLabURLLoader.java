cac20c8f8d2053375256a2fdb5c75a8f485641d3
dont-you <mihailwhoss@gmail.com>
2024-05-19 18:31:35 +0300

    feat: add GitLabURLLoader

--- src/main/java/ru/fusionsoft/dereferencer/git/GitLabURLLoader.java
+++ src/main/java/ru/fusionsoft/dereferencer/git/GitLabURLLoader.java
@@ -20,25 +20,24 @@ public class GitLabURLLoader implements URLLoader{
         configureGitLabLoader("", "https://gitlab.com");
     }
 
-//    @Override
-//    protected InputStream openStream(URI retrieval) throws IOException {
-//        String[] segments = retrieval.getPath().split("/", 7);
-//        String projectPath = segments[1] + "/" + segments[2];
-//        String ref = segments[5];
-//        String filePath = segments[6];
-//        return getFile(projectPath, filePath, ref);
-//    }
-//
-//    private InputStream getFile(String projectPath, String filePath, String ref) throws IOException {
-//        try {
-//            return new ByteArrayInputStream(
-//                    gitLabApi.getRepositoryFileApi().getFile(projectPath, filePath, ref).getDecodedContentAsBytes());
-//        } catch (GitLabApiException e) {
-//            throw new IOException(
-//                    String.format("error while getting file from gitlab with: \n\tmessage - %s\n\thttp code - %s",
-//                            e.getMessage(), e.getHttpStatus()));
-//        }
-//    }
+    private InputStream openStream(URI retrieval) throws IOException {
+        String[] segments = retrieval.getPath().split("/", 7);
+        String projectPath = segments[1] + "/" + segments[2];
+        String ref = segments[5];
+        String filePath = segments[6];
+        return getFile(projectPath, filePath, ref);
+    }
+
+    private InputStream getFile(String projectPath, String filePath, String ref) throws IOException {
+        try {
+            return new ByteArrayInputStream(
+                    gitLabApi.getRepositoryFileApi().getFile(projectPath, filePath, ref).getDecodedContentAsBytes());
+        } catch (GitLabApiException e) {
+            throw new IOException(
+                    String.format("error while getting file from gitlab with: \n\tmessage - %s\n\thttp code - %s",
+                            e.getMessage(), e.getHttpStatus()));
+        }
+    }
 
     public void configureGitLabLoader(String token, String host) {
         gitLabApi = new GitLabApi(host, token);
@@ -50,7 +49,6 @@ public class GitLabURLLoader implements URLLoader{
 
     @Override
     public Resource load(URI uri) throws IOException {
-        // TODO
-        return null;
+        return new Resource(uri, openStream(uri));
     }
 }
